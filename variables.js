22f38741a6476230da139bc408bdda63e829b1b3
shahariar <shahariar@visena.com>
2024-01-04 08:49:32 +0600

    introducing variables

---
+++ variables.js
@@ -0,0 +1,30 @@
+// Weakly typed: Javascript, python
+// Strongly typed: Java
+
+// Variables -> Containers, Varries
+
+// Data types of variables
+
+// 1. String: "", ''
+let first_name = "Shabab";
+console.log("Contains firstly: ", first_name);
+
+let last_name = 'Ahmed';
+console.log("Contains Secondly: ", last_name);
+
+let full_name = first_name + ' ' +last_name;
+console.log("Full name: ", full_name)
+
+// 2. Number
+let first_num = 10;
+console.log("First num: ", first_num);
+
+// let second_num = 20;
+let second_num = "20"; // Data type preference: String -> Number
+console.log("Second num: ", second_num);
+
+let addition = first_num + second_num;
+console.log("Addition: ", addition);
+
+let multiplication = first_num * second_num;
+console.log("Multiplication: ", multiplication);
